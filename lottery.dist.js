929afa739e6a0dd35582f333640563c66b30b7ad
Matthieu Prat <matthieuprat@gmail.com>
2016-09-25 21:48:05 +0200

    Enable candidate list edition

--- lottery.dist.js
+++ lottery.dist.js
@@ -11,14 +11,22 @@ var shuffle = function shuffle(a) {
 var qp = function qp(p) {
   return decodeURI((new RegExp('[?&#]' + p + '=([^&#]*)').exec(window.location.search || window.location.hash) || [])[1] || '');
 };
-var target = document.getElementById('target');
+var candidateElement = document.getElementById('candidate');
+var targetElement = document.getElementById('target');
+var cornerElement = document.getElementById('corner');
+var candidatesElement = document.getElementById('candidates');
+var SYMBOLS = {
+  pencil: '&#x270e;',
+  close: '&times;'
+};
 
+var editMode = false;
 var candidates = (qp('candidates') || 'Yes|No').split('|');
-var freewheel = undefined,
-    running = undefined;
-var limit = undefined,
-    i = undefined,
-    j = undefined;
+var freewheel = void 0,
+    running = void 0;
+var limit = void 0,
+    i = void 0,
+    j = void 0;
 
 function start() {
   if (running) {
@@ -30,34 +38,54 @@ function start() {
   i = j = 0;
   candidates = shuffle(candidates);
   limit = 20 + Math.random() * 5;
-  target.classList.remove('highlight');
+  candidateElement.classList.remove('highlight');
   next();
 }
 
 function next() {
   if (i > limit) return setTimeout(end, 100);
   if (freewheel) i++;
-  target.innerHTML = candidates[j++ % candidates.length];
+  candidateElement.innerHTML = candidates[j++ % candidates.length];
   setTimeout(next, timeout(i));
 }
 
 function end() {
   running = false;
-  target.classList.add('highlight');
+  candidateElement.classList.add('highlight');
 }
 
-;['mousedown', 'touchstart', 'keydown'].forEach(function (e) {
-  return document.addEventListener(e, function (e) {
-    if (e instanceof MouseEvent || e.keyCode === 0 || e.keyCode === 32) {
-      e.preventDefault();
-      start();
-    }
+cornerElement.addEventListener('click', function (e) {
+  if (editMode) {
+    candidates = candidatesElement.value.split('\n');
+    location.hash = 'candidates=' + encodeURI(candidates.join('|'));
+    candidateElement.innerHTML = 'Press the space key';
+  } else {
+    candidatesElement.value = (qp('candidates') || 'Yes|No').split('|').join('\n');
+  }
+  editMode = !editMode;
+  cornerElement.innerHTML = editMode ? SYMBOLS.close : SYMBOLS.pencil;
+  targetElement.style.display = editMode ? 'none' : 'block';
+  candidatesElement.style.display = editMode ? 'block' : 'none';
+});['mousedown', 'touchstart'].forEach(function (e) {
+  return targetElement.addEventListener(e, function () {
+    start();
   });
-});['mouseup', 'touchend', 'keyup'].forEach(function (e) {
-  return document.addEventListener(e, function (e) {
-    if (e instanceof MouseEvent || e.keyCode === 0 || e.keyCode === 32) {
-      e.preventDefault();
-      freewheel = true;
-    }
+});
+document.addEventListener('keydown', function (e) {
+  if (editMode) return;
+  if (e.keyCode === 0 || e.keyCode === 32) {
+    e.preventDefault();
+    start();
+  }
+});['mouseup', 'touchend'].forEach(function (e) {
+  return targetElement.addEventListener(e, function () {
+    freewheel = true;
   });
+});
+document.addEventListener('keyup', function (e) {
+  if (editMode) return;
+  if (e.keyCode === 0 || e.keyCode === 32) {
+    e.preventDefault();
+    freewheel = true;
+  }
 });
--- lottery.js
+++ lottery.js
@@ -1,8 +1,16 @@
 const timeout = i => 90 + 1000 * Math.pow(i / 50, 2)
 const shuffle = a => a.reduce((a, e) => (a.splice(Math.random() * (a.length + 1), 0, e), a), [])
 const qp = p => decodeURI((new RegExp(`[?&#]${p}=([^&#]*)`).exec(window.location.search || window.location.hash) || [])[1] || '')
-const target = document.getElementById('target')
+const candidateElement = document.getElementById('candidate')
+const targetElement = document.getElementById('target')
+const cornerElement = document.getElementById('corner')
+const candidatesElement = document.getElementById('candidates')
+const SYMBOLS = {
+  pencil: '&#x270e;',
+  close: '&times;'
+}
 
+let editMode = false
 let candidates = (qp('candidates') || 'Yes|No').split('|')
 let freewheel, running
 let limit, i, j
@@ -17,36 +25,58 @@ function start () {
   i = j = 0
   candidates = shuffle(candidates)
   limit = 20 + Math.random() * 5
-  target.classList.remove('highlight')
+  candidateElement.classList.remove('highlight')
   next()
 }
 
 function next () {
   if (i > limit) return setTimeout(end, 100)
   if (freewheel) i++
-  target.innerHTML = candidates[j++ % candidates.length]
+  candidateElement.innerHTML = candidates[j++ % candidates.length]
   setTimeout(next, timeout(i))
 }
 
 function end () {
   running = false
-  target.classList.add('highlight')
+  candidateElement.classList.add('highlight')
 }
 
-;['mousedown', 'touchstart', 'keydown'].forEach(e =>
-  document.addEventListener(e, e => {
-    if (e instanceof MouseEvent || e.keyCode === 0 || e.keyCode === 32) {
-      e.preventDefault()
-      start()
-    }
-  })
-)
+cornerElement.addEventListener('click', e => {
+  if (editMode) {
+    candidates = candidatesElement.value.split('\n')
+    location.hash = `candidates=${encodeURI(candidates.join('|'))}`
+    candidateElement.innerHTML = 'Press the space key'
+  } else {
+    candidatesElement.value = (qp('candidates') || 'Yes|No').split('|').join('\n')
+  }
+  editMode = !editMode
+  cornerElement.innerHTML = editMode ? SYMBOLS.close : SYMBOLS.pencil
+  targetElement.style.display = editMode ? 'none' : 'block'
+  candidatesElement.style.display = editMode ? 'block' : 'none'
+})
 
-;['mouseup', 'touchend', 'keyup'].forEach(e =>
-  document.addEventListener(e, e => {
-    if (e instanceof MouseEvent || e.keyCode === 0 || e.keyCode === 32) {
-      e.preventDefault()
-      freewheel = true
-    }
+;['mousedown', 'touchstart'].forEach(e =>
+  targetElement.addEventListener(e, () => {
+    start()
   })
 )
+document.addEventListener('keydown', e => {
+  if (editMode) return
+  if (e.keyCode === 0 || e.keyCode === 32) {
+    e.preventDefault()
+    start()
+  }
+})
+
+;['mouseup', 'touchend'].forEach(e =>
+  targetElement.addEventListener(e, () => {
+    freewheel = true
+  })
+)
+document.addEventListener('keyup', e => {
+  if (editMode) return
+  if (e.keyCode === 0 || e.keyCode === 32) {
+    e.preventDefault()
+    freewheel = true
+  }
+})
